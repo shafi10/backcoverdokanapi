6cd67da4c80716682395fe33acacf854b97cd7f4
shafi2t5 <shafiul.islam@brainstation-23.com>
2022-12-03 16:13:39 +0600

    deployment

--- config/index.ts
+++ config/index.ts
@@ -1,6 +1,7 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
 
+//all config variables
 export const config = {
   dbURL: process.env.MONGODB_URI || 'mongodb://localhost:27017/medEasy',
   jwtSecret: 'jwtSecret',

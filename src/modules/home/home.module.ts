4a4f46fd901470e542a43e66bd049c117fb84e67
shafi2t5 <shafiul.islam@brainstation-23.com>
2022-12-03 17:23:08 +0600

    home api

--- src/modules/home/home.module.ts
+++ src/modules/home/home.module.ts
@@ -1,12 +1,7 @@
 import { Module } from '@nestjs/common';
 import { HomeController } from './home.controller';
-import { MongooseModule } from '@nestjs/mongoose';
-import { Address, AddressSchema } from '../../schemas/address.schema';
 
 @Module({
-  imports: [
-    MongooseModule.forFeature([{ name: Address.name, schema: AddressSchema }]),
-  ],
   controllers: [HomeController],
   providers: [],
 })

559b9e62872dbb7846870f36e6154634dab780bb
shafi2t5 <shafiul.islam@brainstation-23.com>
2022-12-05 13:13:27 +0600

    api deployment

--- config/index.ts
+++ config/index.ts
@@ -5,5 +5,5 @@ dotenv.config();
 export const config = {
   dbURL: process.env.MONGODB_URI || 'mongodb://localhost:27017/medEasy',
   jwtSecret: 'jwtSecret',
-  apiUrl: 'https://bazarapi.onrender.com',
+  apiUrl: process.env.API_URL || 'https://ecom-back-cover.onrender.com',
 };

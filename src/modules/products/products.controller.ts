e955d684f22f4783c4ccf728ec02c7eb97acf96c
shafi2t5 <shafiul.islam@brainstation-23.com>
2022-12-14 15:02:07 +0600

    upload image api

--- src/modules/products/products.controller.ts
+++ src/modules/products/products.controller.ts
@@ -73,23 +73,23 @@ export class ProductsController {
     return this.prodService.disableProduct(id);
   }
 
-  @UseInterceptors(
-    FilesInterceptor('files', 10, {
-      storage: diskStorage({
-        destination: './images',
-        filename: (req, file, cb) => {
-          const fileNameSplit = file.originalname.split('.');
-          const fileExt = fileNameSplit[fileNameSplit.length - 1];
-          cb(null, `${Date.now()}.${fileExt}`);
-        },
-      }),
-    }),
-  )
-  @Post('upload/:id')
-  uploadFile(
-    @UploadedFiles() files: Array<Express.Multer.File>,
-    @Param('id') id: string,
-  ) {
-    return this.prodService.updateProductImage(files, id);
-  }
+  // @UseInterceptors(
+  //   FilesInterceptor('files', 10, {
+  //     storage: diskStorage({
+  //       destination: './images',
+  //       filename: (req, file, cb) => {
+  //         const fileNameSplit = file.originalname.split('.');
+  //         const fileExt = fileNameSplit[fileNameSplit.length - 1];
+  //         cb(null, `${Date.now()}.${fileExt}`);
+  //       },
+  //     }),
+  //   }),
+  // )
+  // @Post('upload/:id')
+  // uploadFile(
+  //   @UploadedFiles() files: Array<Express.Multer.File>,
+  //   @Param('id') id: string,
+  // ) {
+  //   return this.prodService.updateProductImage(files, id);
+  // }
 }

f68854f691b1397cfb7ced3808a4157c0b462fb6
shafi2t5 <shafiul.islam@brainstation-23.com>
2022-12-15 13:02:39 +0600

    cros issue solve

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,18 @@ import { NestFactory } from '@nestjs/core';
 // import { connectToDatabase } from 'utils/connect';
 import { AppModule } from './app.module';
 
+const options = {
+  origin: '*',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+  credentials: true,
+};
+
 async function bootstrap() {
   // await connectToDatabase();
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    allowedHeaders: '*',
-    origin: '*',
-    credentials: true,
-  });
+  app.enableCors(options);
   app.useGlobalPipes(new ValidationPipe());
   const Port = parseInt(process.env.PORT) || 3001;
   await app.listen(Port);
